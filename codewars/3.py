53ff5e24f037bad3d3c0a12b3a0077ca3c1cae24
Jacek Ostański <jacek@ivolution.pl>
2023-10-18 09:44:54 +0200

    Create 3.py

---
+++ codewars/3.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/python
+# Solution
+def create_phone_number(n):
+    n = [str(x) for x in n]
+    return f"({''.join(n[0:3])}) {''.join(n[3:6])}-{''.join(n[6:])}"
