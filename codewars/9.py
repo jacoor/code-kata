3debab176021436710564b98b9aa6cc62f872535
Jacek Ostański <jacek@ivolution.pl>
2023-10-26 10:47:11 +0200

    Create 9.py

---
+++ codewars/9.py
@@ -0,0 +1,17 @@
+"""
+https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/python
+
+In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+
+The string has the following conditions to be alphanumeric:
+
+At least one character ("" is not valid)
+Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+No whitespaces / underscore
+
+"""
+
+import re
+
+def alphanumeric(password):
+    return True if re.match('^[a-zA-Z0-9]+$',password) else False
